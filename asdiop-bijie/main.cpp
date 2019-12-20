cb10cdb79d3d27d6c4024f88925b9aa96f01e8c8
tzf-omeky <18156283027@163.com>
2019-12-20 13:30:02 +0800

    init

---
+++ asdiop-bijie/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int myFunc(int s1,int s2,int s3,int s4)
+{
+	int e = 0;
+	e = s1 + s2 + s3 + s4;
+	return e;
+}
+
+int main()
+{
+	int a = 0x50;
+	int b = 0x100;
+	myFunc(a, b, 0x200, 0x300);
+	printf("hello asdiop\n");
+	return 0;
+}
